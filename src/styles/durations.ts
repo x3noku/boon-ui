f222974572dabda580e048ea2bf0a371edcef51c
x3noku <x3noku@gmail.com>
2021-07-07 11:47:11 +0600

    Add Styles Files commit

---
+++ src/styles/durations.ts
@@ -0,0 +1,7 @@
+export default {
+    VERY_LONG: 800,
+    LONG: 600,
+    MEDIUM: 400,
+    SHORT: 200,
+    VERY_SHORT: 120,
+};
---
+++ src/styles/sizes.ts
@@ -0,0 +1,7 @@
+export default {
+    TEXT_LARGE: 36,
+    TEXT_BIG: 22,
+    TEXT_MEDIUM: 20,
+    TEXT_SMALL: 16,
+    TEXT_LITTLE: 14,
+};
