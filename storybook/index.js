78a781e24d08d3cf4b4714d2b447fd98ca240800
x3noku <x3noku@gmail.com>
2021-07-07 11:25:21 +0600

    Install StoryLoader commit

--- storybook/index.js
+++ storybook/index.js
@@ -1,18 +1,14 @@
 import { getStorybookUI, configure, addDecorator } from '@storybook/react-native';
 import { withKnobs } from '@storybook/addon-knobs';
-
+import { loadStories } from './storyLoader';
 import './rn-addons';
 
-// enables knobs for all stories
 addDecorator(withKnobs);
 
-// import stories
 configure(() => {
-    require('./stories');
+    loadStories();
 }, module);
 
-// Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
-// To find allowed options for getStorybookUI
 const StorybookUIRoot = getStorybookUI({});
 
 export default StorybookUIRoot;
