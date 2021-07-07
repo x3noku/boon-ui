7697172b3a2d41a5fb9d3ce83866bdf15b012bfd
x3noku <x3noku@gmail.com>
2021-07-07 11:49:58 +0600

    Configure Path Aliases commit

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,21 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin']
-  };
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            'react-native-reanimated/plugin',
+            [
+                'module-resolver',
+                {
+                    root: ['./'],
+                    alias: {
+                        '@assets': './src/assets',
+                        '@components': './src/components',
+                        '@styles': './src/styles',
+                        '@utils': './src/utils',
+                    },
+                },
+            ],
+        ],
+    };
 };
